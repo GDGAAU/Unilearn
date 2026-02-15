97df023d6ebfdebcbe84fdb003cd425af45b6d48
dagm <dagimyibabe19@gmail.com>
2026-02-15 10:36:20 +0300

    feat: update database settings to use PostgreSQL with environment variables

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -92,12 +92,20 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "unilearn_db"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
+
+
 
 
 # Password validation
