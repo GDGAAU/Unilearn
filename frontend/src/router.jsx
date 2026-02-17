38d68a2ef35fa1943fc1fba712457c60d8e01ce9
Fenet <fenetasmamaw48@gmail.com>
2026-02-17 12:31:38 +0300

    implement signup UI structure

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Signup from "./pages/Register/Signup";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+    {
+    path: "/signup",
+    element: <Signup />,     
+  }
 ]);
 
 export default router;
