51d20541377ae091483d85c9f01edf3421ac2b4a
BeamlakF <beamlakfekadu562@gmail.com>
2026-03-19 13:10:07 +0300

    urls

--- unibackend/courses/models.py
+++ unibackend/courses/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 
 class Course(models.Model):
-    course_code = models.CharField(max_length=50, unique=True)
     course_name = models.CharField(max_length=255)
     department = models.CharField(max_length=255)
-    year = models.PositiveSmallIntegerField()
-    description = models.TextField()
+    year = models.CharField(max_length= 100)
 
     def __str__(self):
         return f"{self.course_code} - {self.course_name}"
--- unibackend/instructors/urls.py
+++ unibackend/instructors/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # Admin
-    path("admin/", views.AdminInstructorListCreateView.as_view()),
-    path("admin/<int:pk>/", views.AdminInstructorDetailView.as_view()),
-    path("admin/insights/<int:pk>/approve/", views.AdminInstructorInsightApproveView.as_view()),
+    path("/admin/instructor/", views.AdminInstructorListCreateView.as_view()),
+    path("/admin/<int:pk>/", views.AdminInstructorDetailView.as_view()),
+    path("/admin/insights/<int:pk>/approve/", views.AdminInstructorInsightApproveView.as_view()),
 
     # Public
-    path("<int:instructor_id>/insights/", views.InstructorInsightCreateView.as_view()),
-    path("<int:instructor_id>/insights/list/", views.InstructorInsightListView.as_view()),
+    path("/<int:instructor_id>/insights/", views.InstructorInsightCreateView.as_view()),
+    path("/<int:instructor_id>/insights/list/", views.InstructorInsightListView.as_view()),
 ]
